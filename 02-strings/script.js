b013cc3fbf8b1573fa84e9151aa243f2a3229270
mbaisotti99 <matteo.baisotti@gmail.com>
2024-10-25 13:40:11 +0200

    strings

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -2,10 +2,22 @@
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
 
+let name = "Matteo";
+console.log(`Il mio nome è ${name}`);
+
+
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+
+
+let number = 12;
+let text = "Scritto";
+let bool = false ;
+
+console.log(`Il numero è ${number}, il testo è ${text}, mentre il bool è ${bool}`)
+
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
@@ -14,10 +26,10 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = firstName + " " + lastName;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
-console.log(numberOfLettersInAlphabet); // Output: 26
+const numberOfLettersInAlphabet = alphabet.length;
+console.log(`Ci sono ${numberOfLettersInAlphabet} lettere nell'alfabeto`); // Output: 26
